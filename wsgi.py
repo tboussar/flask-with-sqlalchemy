1d1a5c2f7b4a49fd719136012412731c6e422643
tboussar <thombouss@hotmail.com>
2018-07-12 11:38:30 +0200

    Fist Depl to Heroku

---
+++ wsgi.py
@@ -0,0 +1,29 @@
+# wsgi.py
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass # Heroku does not use .env
+
+from flask import Flask
+from config import Config
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow # Order is important here!
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+from models import Product
+from schemas import products_schema
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/api/v1/products')
+def products():
+    products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+    return products_schema.jsonify(products)
